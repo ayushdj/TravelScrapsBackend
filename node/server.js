0a885c13b739cbf855a6cf42b7f996572f8fd21b
ayushdj <ayushdj13@gmail.com>
2021-12-03 09:41:42 -0500

    added function to delete profile

--- node/server.js
+++ node/server.js
@@ -22,12 +22,9 @@ app.use(function(req, res, next) {
 
 
 require('./db/carousel/service')(app);
-
 require('./db/signUp/service')(app);
 //require('./db/persons/service')(app);
 require('./db/countdown/service')(app);
 require('./db/who/who-service')(app);
-
-
 
 app.listen(4000);
