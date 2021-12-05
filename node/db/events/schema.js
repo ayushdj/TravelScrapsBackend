cbe36899e726e204b31ff217454a4d53b81975eb
priyalpat721 <priyalpat721@gmail.com>
2021-12-05 13:18:05 -0500

    changed events scehma

--- node/db/events/schema.js
+++ node/db/events/schema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     date: String,
-    travelPlan: Array,
-    eventName: String
+    event: String
 }, {collection: 'events'});
 module.exports = schema;
