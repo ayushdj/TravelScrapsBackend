8b76a92d88531efe9d679b6de07040a078df7963
ayushdj <ayushdj13@gmail.com>
2021-12-08 17:20:34 -0500

    fixed backend for settings and profile

--- node/db/signUp/service.js
+++ node/db/signUp/service.js
@@ -23,38 +23,8 @@ module.exports = (app) => {
             .then(profile => res.json(profile[0]));
 
 
-    const login = (req, res) => {
-        dao.findProfileByUsername(req.body)
-            .then(user => {
-                if(user) {
-                    req.session['currUser'] = user;
-                    res.json(user);
-                    return;
-                }
-                res.sendStatus(403);
-            })
-    }
-
-    const register = (req, res) => {
-        dao.findProfileByUsername(req.body)
-            .then(user => {
-                if(user) {
-                    res.sendStatus(404);
-                    return;
-                }
-                createPerson(req.body)
-                    .then(user => {
-                        req.session['currUser'] = user;
-                        res.json(user)
-                    });
-            })
-    }
-
-    const getCurrentUser = (req, res) =>
-        res.json(req.session['currUser']);
-
-    const logout = (req, res) =>
-        res.send(req.session.destroy());
+    const deleteProfile = (req, res) =>
+        dao.deleteProfileById(req.params.id).then((status) => res.send(status));
 
     // app.post('/api/login', login);
     // app.post('/api/register', register);
@@ -65,4 +35,5 @@ module.exports = (app) => {
     app.get("/db/person/:id", findProfileById);
     app.put('/db/person/:id', updateProfile);
     app.get("/db/person/:username/:password", findProfileByUsername);
+    app.delete("/db/person/:id", deleteProfile)
 }
