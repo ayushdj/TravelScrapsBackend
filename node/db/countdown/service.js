812c98d75c11ac368262ac19844d402064f870dc
hearan-won <54459621+hearan-won@users.noreply.github.com>
2021-12-07 19:45:28 -0500

    commit

--- node/db/countdown/service.js
+++ node/db/countdown/service.js
@@ -2,6 +2,13 @@ const dao = require('./dao')
 
 module.exports = (app) => {
 
+    const createCountDown = (req, res) =>
+        dao.createCountDown(req.body)
+            .then((countdown) => res.json(countdown));
+
+    app.post("/db/countdown/service", createCountDown);
+
+
     const updateCountDown = (req, res) =>
         dao.updateCountDown(req.params.id, req.body)
             .then(status => res.send(status));
